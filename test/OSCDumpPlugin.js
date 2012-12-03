37e3d63175652ece10b82766a0c3c801ac66ed84
Cristiano Belloni <janesconference@gmail.com>
2012-12-03 12:59:37 +0100

    Added test/OSCDump plugin

---
+++ test/OSCDumpPlugin.js
@@ -0,0 +1,65 @@
+define(['kievII'], function() {
+  var pluginConf = {
+      canvas: {
+          width: 200,
+          height: 50
+      },
+      osc: true
+  }
+  var initPlugin = function(args) {
+    console.log ("plugin inited, args is", args, "KievII object is ", K2);
+    
+    this.ui = new K2.UI ({type: 'CANVAS2D', target: args.canvas});
+    
+    this.viewWidth = args.canvas.width;
+    this.viewHeight = args.canvas.height;
+    
+    this.handler = args.OSCHandler;
+    
+    var oscCallback = function (message) {
+       console.log ("OSCDumpPlugin received message: ", message);
+       var dest = message.toString();
+       this.ui.setValue({
+            elementID : 'OSCDumpLabel',
+            slot : 'labelvalue',
+            value : dest,
+            fireCallback: false
+        });
+       this.ui.refresh();
+       
+    };
+        
+    this.localClient = this.handler.registerClient ({ clientID : "clickBarPlugin",
+                                                      oscCallback : oscCallback.bind (this)
+                                                    });
+    var fontHeight =  Math.floor(this.viewHeight / 4);
+    
+    // Label
+    var labelArgs = {
+        ID: "OSCDumpLabel",
+        left : 0,
+        top : 0,
+        height: this.viewHeight,
+        width: this.viewWidth,
+        textColor: "black",
+        transparency: 0.8,
+        objParms: {
+            font: fontHeight + "pt Arial",
+            textBaseline: "top",
+            textAlign: "left"
+        },
+        onValueSet: function (slot, value, element) {
+            this.ui.refresh();
+        }.bind(this),
+        isListening: true
+    };
+    
+    this.ui.addElement(new K2.Label(labelArgs));
+    this.ui.refresh();
+    return this;   
+  };
+  return {
+    initPlugin: initPlugin,
+    pluginConf: pluginConf
+  };
+});
