3a805113b4d33dd6e8a90d6099d749deaf1e85a4
Cristiano Belloni <janesconference@gmail.com>
2013-01-14 12:32:57 +0100

    Fixed a typo in external address

--- plugins/Voron/Voron.js
+++ plugins/Voron/Voron.js
@@ -1,4 +1,4 @@
-define(['kievII', 'https://github.com/corbanbrook/dsp.js/raw/master/dsp.js', 'https://github.co/janesconference/KievII/raw/master/dsp/pitchshift.js'/*, require.toUrl('test/testload.js')*/], function() {
+define(['kievII', 'https://github.com/corbanbrook/dsp.js/raw/master/dsp.js', 'https://github.com/janesconference/KievII/raw/master/dsp/pitchshift.js'/*, require.toUrl('test/testload.js')*/], function() {
   var pluginConf = {
       osc: true,
       audioIn: true,
