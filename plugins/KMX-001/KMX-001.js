aab6c17ea7bac593a0974da770413ddc8a9d7beb
Cristiano Belloni <janesconference@gmail.com>
2013-02-06 12:36:20 +0100

    KMX, KSP and Voron use a second require to get their resources

--- plugins/KMX-001/KMX-001.js
+++ plugins/KMX-001/KMX-001.js
@@ -1,199 +1,214 @@
-define(['kievII',
-        'image!'+ require.toUrl('assets/images/vsliderhandle_50.png!rel'),
-        'image!'+ require.toUrl('assets/images/vsliderslot_empty.png!rel'),
-        'image!'+ require.toUrl('assets/images/lmh_series.png!rel'),
-        'image!'+ require.toUrl('assets/images/trim_series.png!rel'),
-        'image!'+ require.toUrl('assets/images/KMX-001_deck.png!rel')
-        ], function(k2, faderImg, slotImg, blackKnobImg, redKnobImg, deckImg) {
-        console.log ("***MX: ", require.toUrl('.'));
-        var faderImage =  faderImg;
-        var slotImage =  slotImg;
-        var blackKnobImage =  blackKnobImg;
-        var redKnobImage = redKnobImg;
-        var deckImage = deckImg; 
-        var nOfChannels = 4;
-  var pluginConf = {
-      osc: true,
-      audioIn: nOfChannels,
-      audioOut: 1,
-      canvas: {
-          width: 537,
-          height: 680
-      },
-  }
-  var initPlugin = function(args) {
-    this.name = args.name;
-    this.id = args.id;
-    
-    // The sound part
-    this.audioSources = args.audioSources;
-    this.audioDestination = args.audioDestinations[0];
-    this.context = args.audioContext;
-    var context = this.context;
-    this.gainMixerNodes = [];
-    this.lowFilters = [];
-    this.midFilters = [];
-    this.highFilters = [];
-    this.trimNodes = [];
-    this.masterGainNode = context.createGainNode();
-    
-    for (var i = 0; i < this.audioSources.length; i+=1) {
-        this.gainMixerNodes[i] = context.createGainNode();
-        this.audioSources[i].connect(this.gainMixerNodes[i]);
-        
-        this.lowFilters[i] = context.createBiquadFilter(),
-        this.midFilters[i] = context.createBiquadFilter(),
-        this.highFilters[i] = context.createBiquadFilter();
-    
-         // Set the filter types (you could set all to 5, for a different result, feel free to experiment)
-         // Set the filter gains to  0 (gain = boost in dB)
-         this.lowFilters[i].type = 3;
-         this.lowFilters[i].gain.value = 0;
-         this.lowFilters[i].frequency.value = 80;
-         this.midFilters[i].type = 5;
-         this.midFilters[i].gain.value = 0;
-         this.midFilters[i].frequency.value = 1000;
-         this.highFilters[i].type = 4;
-         this.highFilters[i].frequency.value = 8000;
-         this.highFilters[i].gain.value = 0;
-         
-         this.trimNodes[i] = context.createGainNode();
-         this.trimNodes[i].gain.value = 1;
-        
-         // Create and connect the filter chain
-         this.gainMixerNodes[i].connect(this.lowFilters[i]);
-         this.lowFilters[i].connect(this.midFilters[i]);
-         this.midFilters[i].connect(this.highFilters[i]);
-         this.highFilters[i].connect(this.trimNodes[i]);
-         this.trimNodes[i].connect (this.masterGainNode);
-         this.masterGainNode.connect(this.audioDestination);
-    }
-    
-    // The canvas part
-    this.ui = new K2.UI ({type: 'CANVAS2D', target: args.canvas});
-    
-    this.viewWidth = args.canvas.width;
-    this.viewHeight = args.canvas.height;
-    
-    var idPrefix = "gainFader";
-    
-    var spaceWidth = 131;
-    
-    /* deck */
-   var bgArgs = new K2.Background({
-        ID: 'background',
-        image: deckImage,
-        top: 0,
-        left: 0
-    });
+define(['kievII', 'require'], function(k2, require) {
 
-    this.ui.addElement(bgArgs, {zIndex: 0});
+    /* This gets returned to the host as soon as the plugin is loaded */ 
+    var pluginConf = {
+        osc: true,
+        audioIn: 4,
+        audioOut: 1,
+        canvas: {
+            width: 537,
+            height: 680
+        },
+    };
+  
+    /* This gets called when all the resources are loaded */
+    var pluginFunction = function (args, resources) {
+        this.name = args.name;
+        this.id = args.id;
+        
+        var faderImage =  resources[0];
+        var slotImage =  resources[1];
+        var blackKnobImage =  resources[2];
+        var redKnobImage = resources[3];
+        var deckImage = resources[4]; 
+        
+        var nOfChannels = args.audioSources.length;
+        
+        // The sound part
+        this.audioSources = args.audioSources;
+        this.audioDestination = args.audioDestinations[0];
+        this.context = args.audioContext;
+        var context = this.context;
+        this.gainMixerNodes = [];
+        this.lowFilters = [];
+        this.midFilters = [];
+        this.highFilters = [];
+        this.trimNodes = [];
+        this.masterGainNode = context.createGainNode();
+        
+        for (var i = 0; i < this.audioSources.length; i+=1) {
+            this.gainMixerNodes[i] = context.createGainNode();
+            this.audioSources[i].connect(this.gainMixerNodes[i]);
+            
+            this.lowFilters[i] = context.createBiquadFilter(),
+            this.midFilters[i] = context.createBiquadFilter(),
+            this.highFilters[i] = context.createBiquadFilter();
+        
+             // Set the filter types (you could set all to 5, for a different result, feel free to experiment)
+             // Set the filter gains to  0 (gain = boost in dB)
+             this.lowFilters[i].type = 3;
+             this.lowFilters[i].gain.value = 0;
+             this.lowFilters[i].frequency.value = 80;
+             this.midFilters[i].type = 5;
+             this.midFilters[i].gain.value = 0;
+             this.midFilters[i].frequency.value = 1000;
+             this.highFilters[i].type = 4;
+             this.highFilters[i].frequency.value = 8000;
+             this.highFilters[i].gain.value = 0;
+             
+             this.trimNodes[i] = context.createGainNode();
+             this.trimNodes[i].gain.value = 1;
+            
+             // Create and connect the filter chain
+             this.gainMixerNodes[i].connect(this.lowFilters[i]);
+             this.lowFilters[i].connect(this.midFilters[i]);
+             this.midFilters[i].connect(this.highFilters[i]);
+             this.highFilters[i].connect(this.trimNodes[i]);
+             this.trimNodes[i].connect (this.masterGainNode);
+             this.masterGainNode.connect(this.audioDestination);
+        }
+        
+        // The canvas part
+        this.ui = new K2.UI ({type: 'CANVAS2D', target: args.canvas});
+        
+        this.viewWidth = args.canvas.width;
+        this.viewHeight = args.canvas.height;
+        
+        var idPrefix = "gainFader";
+        
+        var spaceWidth = 131;
+        
+        /* deck */
+       var bgArgs = new K2.Background({
+            ID: 'background',
+            image: deckImage,
+            top: 0,
+            left: 0
+        });
     
-    /* slider */
-    vSliderArgs = {
-            ID: "",
-            left: spaceWidth,
-            top : 448,
-            sliderImg: slotImage,
-            knobImg: faderImage,
-            onValueSet: function (slot, value, element) {
-                var index = element.split(idPrefix)[1];
-                index = parseInt(index, 10);
-                var audioNode = this.gainMixerNodes[index];
-                audioNode.gain.value = 1 - value;
-                this.ui.refresh();
-            }.bind(this),
-            type:"vertical",
-            isListening: true
-        };
-    
-    for (var i = 0; i < nOfChannels; i += 1) {
-        vSliderArgs.ID = idPrefix + i;
-        vSliderArgs.left = (43 +  i * spaceWidth);
-        this.ui.addElement(new K2.Slider(vSliderArgs));
-        this.ui.setValue ({elementID: vSliderArgs.ID, value: 0.5});
-    }
-    
-    /* Channel knobs */
-    var knobArgs = {
-         ID: "",
-         left: spaceWidth,
-         top: 0,
-         sensitivity : 5000,
-         tileWidth: 50,
-         tileHeight: 50,
-         imageNum: 50,
-         bottomAngularOffset: 33,
-         onValueSet: function (slot, value, element) {
-             var index, realValue;
-             if (element.indexOf("lowKnob") === 0) {
-                 // Filter gain
-                 realValue = K2.MathUtils.linearRange (0, 1, -12, 12, value);
-                 index = element.split("lowKnob")[1];
-                 index = parseInt(index, 10);
-                 this.lowFilters[index].gain.value = realValue;
-                 console.log ("L: index, value, realValue", index, value, realValue);
-             }
-             else if (element.indexOf("midKnob") === 0) {
-                 // Filter gain
-                 realValue = K2.MathUtils.linearRange (0, 1, -12, 12, value);
-                 index = element.split("midKnob")[1];
-                 index = parseInt(index, 10);
-                 this.midFilters[index].gain.value = realValue;
-                 console.log ("M: index, value, realValue", index, value, realValue);
-             }
-             else if (element.indexOf("hiKnob") === 0) {
-                 // Filter gain
-                 realValue = K2.MathUtils.linearRange (0, 1, -12, 12, value);
-                 index = element.split("hiKnob")[1];
-                 index = parseInt(index, 10);
-                 this.highFilters[index].gain.value = realValue;
-                 console.log ("H: index, value, realValue", index, value, realValue);
-             }
-             else if (element.indexOf("trimKnob") === 0) {
-                 // Trim gain
-                 if (value < 0.5) {
-                     realValue = K2.MathUtils.linearRange (0, 0.5, 0, 1, value);
+        this.ui.addElement(bgArgs, {zIndex: 0});
+        
+        /* slider */
+        vSliderArgs = {
+                ID: "",
+                left: spaceWidth,
+                top : 448,
+                sliderImg: slotImage,
+                knobImg: faderImage,
+                onValueSet: function (slot, value, element) {
+                    var index = element.split(idPrefix)[1];
+                    index = parseInt(index, 10);
+                    var audioNode = this.gainMixerNodes[index];
+                    audioNode.gain.value = 1 - value;
+                    this.ui.refresh();
+                }.bind(this),
+                type:"vertical",
+                isListening: true
+            };
+        
+        for (var i = 0; i < nOfChannels; i += 1) {
+            vSliderArgs.ID = idPrefix + i;
+            vSliderArgs.left = (43 +  i * spaceWidth);
+            this.ui.addElement(new K2.Slider(vSliderArgs));
+            this.ui.setValue ({elementID: vSliderArgs.ID, value: 0.5});
+        }
+        
+        /* Channel knobs */
+        var knobArgs = {
+             ID: "",
+             left: spaceWidth,
+             top: 0,
+             sensitivity : 5000,
+             tileWidth: 50,
+             tileHeight: 50,
+             imageNum: 50,
+             bottomAngularOffset: 33,
+             onValueSet: function (slot, value, element) {
+                 var index, realValue;
+                 if (element.indexOf("lowKnob") === 0) {
+                     // Filter gain
+                     realValue = K2.MathUtils.linearRange (0, 1, -12, 12, value);
+                     index = element.split("lowKnob")[1];
+                     index = parseInt(index, 10);
+                     this.lowFilters[index].gain.value = realValue;
+                     console.log ("L: index, value, realValue", index, value, realValue);
+                 }
+                 else if (element.indexOf("midKnob") === 0) {
+                     // Filter gain
+                     realValue = K2.MathUtils.linearRange (0, 1, -12, 12, value);
+                     index = element.split("midKnob")[1];
+                     index = parseInt(index, 10);
+                     this.midFilters[index].gain.value = realValue;
+                     console.log ("M: index, value, realValue", index, value, realValue);
+                 }
+                 else if (element.indexOf("hiKnob") === 0) {
+                     // Filter gain
+                     realValue = K2.MathUtils.linearRange (0, 1, -12, 12, value);
+                     index = element.split("hiKnob")[1];
+                     index = parseInt(index, 10);
+                     this.highFilters[index].gain.value = realValue;
+                     console.log ("H: index, value, realValue", index, value, realValue);
+                 }
+                 else if (element.indexOf("trimKnob") === 0) {
+                     // Trim gain
+                     if (value < 0.5) {
+                         realValue = K2.MathUtils.linearRange (0, 0.5, 0, 1, value);
+                     }
+                     else {
+                        realValue = K2.MathUtils.linearRange (0.5, 1, 1, 12, value);
+                     }
+                     index = element.split("trimKnob")[1];
+                     index = parseInt(index, 10);
+                     this.trimNodes[index].gain.value = realValue;
+                     console.log ("T: index, value, realValue", index, value, realValue);
                  }
                  else {
-                    realValue = K2.MathUtils.linearRange (0.5, 1, 1, 12, value);
+                     console.error ("Cannot parse element", element);
                  }
-                 index = element.split("trimKnob")[1];
-                 index = parseInt(index, 10);
-                 this.trimNodes[index].gain.value = realValue;
-                 console.log ("T: index, value, realValue", index, value, realValue);
-             }
-             else {
-                 console.error ("Cannot parse element", element);
-             }
-             this.ui.refresh();
-         }.bind(this),
-         isListening: true
-     };
-     
-     var id_lmh = ["trimKnob", "hiKnob", "midKnob", "lowKnob"];
-     
-     for (var knobtype = 0; knobtype < id_lmh.length; knobtype += 1) {
-         knobArgs.top = 96 + 86 * knobtype;
-         if (id_lmh[knobtype] === "trimKnob") {
-                knobArgs.imagesArray = [redKnobImage];
+                 this.ui.refresh();
+             }.bind(this),
+             isListening: true
+         };
+         
+         var id_lmh = ["trimKnob", "hiKnob", "midKnob", "lowKnob"];
+         
+         for (var knobtype = 0; knobtype < id_lmh.length; knobtype += 1) {
+             knobArgs.top = 96 + 86 * knobtype;
+             if (id_lmh[knobtype] === "trimKnob") {
+                    knobArgs.imagesArray = [redKnobImage];
+                }
+                else {
+                    knobArgs.imagesArray = [blackKnobImage];
+                }
+             for (i = 0; i < nOfChannels; i += 1) {
+                knobArgs.ID = id_lmh[knobtype] + i;
+                knobArgs.left = (45 + i * spaceWidth);
+                this.ui.addElement(new K2.Knob(knobArgs));
+                this.ui.setValue ({elementID: knobArgs.ID, value: 0.5});
             }
-            else {
-                knobArgs.imagesArray = [blackKnobImage];
-            }
-         for (i = 0; i < nOfChannels; i += 1) {
-            knobArgs.ID = id_lmh[knobtype] + i;
-            knobArgs.left = (45 + i * spaceWidth);
-            this.ui.addElement(new K2.Knob(knobArgs));
-            this.ui.setValue ({elementID: knobArgs.ID, value: 0.5});
         }
-    }
-    
-     this.ui.refresh();        
+        
+        this.ui.refresh(); 
+  }
+  
+    /* This function gets called by the host every time an instance of
+       the plugin is requested [e.g: displayed on screen] */        
+    var initPlugin = function(initArgs) {
+        var args = initArgs;
+        require ([  'image!'+ require.toUrl('./assets/images/vsliderhandle_50.png'),
+                    'image!'+ require.toUrl('./assets/images/vsliderslot_empty.png'),
+                    'image!'+ require.toUrl('./assets/images/lmh_series.png'),
+                    'image!'+ require.toUrl('./assets/images/trim_series.png'),
+                    'image!'+ require.toUrl('./assets/images/KMX-001_deck.png')],
+                    function () {
+                        var resources = arguments;
+                        pluginFunction.call (this, args, resources);
+                    }.bind(this)
+                );
+    };
       
-  };
-  return {
-    initPlugin: initPlugin,
-    pluginConf: pluginConf
-  };
+    return {
+        initPlugin: initPlugin,
+        pluginConf: pluginConf
+    };
 });
--- plugins/Voron/Voron.js
+++ plugins/Voron/Voron.js
@@ -1,134 +1,149 @@
-define(['kievII',
-        'https://github.com/corbanbrook/dsp.js/raw/master/dsp.js',
-        'https://github.com/janesconference/KievII/raw/master/dsp/pitchshift.js',
-        'image!'+ require.toUrl('assets/images/Voron_bg2.png!rel'),
-        'image!'+ require.toUrl('assets/images/white_big.png!rel')],
-        function(k2, dsp, pitchshift, bgImage, kImage) {
-
-  var backgroundImage =  bgImage;
-  var knobImage =  kImage;
-  var nSamples = 2048;
-  var fftFrameSize = 2048;
-  shifterStartValue = 0;
+define(['kievII', 'require'], function(k2, require) {
   
-  var pluginConf = {
-      osc: true,
-      audioIn: 1,
-      audioOut: 1,
-      canvas: {
-          width: 268,
-          height: 340
-      },
-  }
-  var initPlugin = function(args) {
-    console.log ("plugin inited, args is", args, "KievII object is ", K2, "bg image is", backgroundImage);
-    
-    this.name = args.name;
-    this.id = args.id;
-    
-    // The sound part
-    this.audioSource = args.audioSources[0];
-    this.audioDestination = args.audioDestinations[0];
-    this.context = args.audioContext;
-    var context = this.context;
-    
-    this.processorNode = this.context.createJavaScriptNode(nSamples, 1, 1);
-    
-    this.shifter = new Pitchshift(fftFrameSize, this.context.sampleRate, 'FFT');
-    
-    this.processorNode.onaudioprocess = function (event) {
-        // Get left/right input and output arrays
-        var outputArray = [];
-        outputArray[0] = event.outputBuffer.getChannelData(0);
-        var inputArray = [];
-        inputArray[0] = event.inputBuffer.getChannelData(0);
-        // console.log ("input is long: ", inputArray[0].length);
-        var data = inputArray[0];
-        this.shifter.process (this.shiftValue, data.length, 4, data);
-        
-        var out_data = outputArray[0];
-        for (i = 0; i < out_data.length; ++i) {
-            out_data[i] = this.shifter.outdata[i];
-        }
-        
-    }.bind(this);
-    
-    this.audioSource.connect (this.processorNode);
-    this.processorNode.connect (this.audioDestination);
-    
-    // The OSC part
-    this.OSChandler = args.OSCHandler;
-     
-    var oscCallback = function (message) {
-       console.log (this.id + " received message: ", message);
-       var dest = message[0];
-       if (dest === this.id + '/bypass/set/') {
-           var bypass = message[1];
-           if (bypass === true) {
-               //TODO
-           }
-           else if (bypass === false) {
-               //TODO
-           }
-           else {
-               console.error ("Bypass value not known: ", bypass);
-           }
-        }
-    };
-    
-    this.localClient = this.OSChandler.registerClient ({ clientID : this.id,
-                                                      oscCallback : oscCallback.bind (this)
-                                                    });
-    
-     // The graphical part
-    this.ui = new K2.UI ({type: 'CANVAS2D', target: args.canvas});
-    
-    this.viewWidth = args.canvas.width;
-    this.viewHeight = args.canvas.height;
-	
-    /* BACKGROUND INIT */
-	
-    var bg = new K2.Background({
-        ID: 'background',
-        image: backgroundImage,
-        top: 0,
-        left: 0
-    });
+    /* This gets returned to the host as soon as the plugin is loaded */ 
+    var pluginConf = {
+        osc: true,
+        audioIn: 1,
+        audioOut: 1,
+        canvas: {
+            width: 268,
+            height: 340
+        },
+    }
 
-    this.ui.addElement(bg, {zIndex: 0});
+    /* This gets called when all the resources are loaded */
+    var pluginFunction = function (args, resources) {
+      
+        var backgroundImage = resources[2];
+        var knobImage = resources[3];
+        var nSamples = 2048;
+        var fftFrameSize = 2048;
+        shifterStartValue = 0;
+        
+        console.log ("plugin inited, args is", args, "KievII object is ", K2, "bg image is", backgroundImage);
+        
+        this.name = args.name;
+        this.id = args.id;
+        
+        // The sound part
+        this.audioSource = args.audioSources[0];
+        this.audioDestination = args.audioDestinations[0];
+        this.context = args.audioContext;
+        var context = this.context;
+        
+        this.processorNode = this.context.createJavaScriptNode(nSamples, 1, 1);
+        
+        this.shifter = new Pitchshift(fftFrameSize, this.context.sampleRate, 'FFT');
+        
+        this.processorNode.onaudioprocess = function (event) {
+            // Get left/right input and output arrays
+            var outputArray = [];
+            outputArray[0] = event.outputBuffer.getChannelData(0);
+            var inputArray = [];
+            inputArray[0] = event.inputBuffer.getChannelData(0);
+            // console.log ("input is long: ", inputArray[0].length);
+            var data = inputArray[0];
+            this.shifter.process (this.shiftValue, data.length, 4, data);
+            
+            var out_data = outputArray[0];
+            for (i = 0; i < out_data.length; ++i) {
+                out_data[i] = this.shifter.outdata[i];
+            }
+            
+        }.bind(this);
+        
+        this.audioSource.connect (this.processorNode);
+        this.processorNode.connect (this.audioDestination);
+        
+        // The OSC part
+        this.OSChandler = args.OSCHandler;
+         
+        var oscCallback = function (message) {
+           console.log (this.id + " received message: ", message);
+           var dest = message[0];
+           if (dest === this.id + '/bypass/set/') {
+               var bypass = message[1];
+               if (bypass === true) {
+                   //TODO
+               }
+               else if (bypass === false) {
+                   //TODO
+               }
+               else {
+                   console.error ("Bypass value not known: ", bypass);
+               }
+            }
+        };
+        
+        this.localClient = this.OSChandler.registerClient ({ clientID : this.id,
+                                                          oscCallback : oscCallback.bind (this)
+                                                        });
+        
+         // The graphical part
+        this.ui = new K2.UI ({type: 'CANVAS2D', target: args.canvas});
+        
+        this.viewWidth = args.canvas.width;
+        this.viewHeight = args.canvas.height;
+        
+        /* BACKGROUND INIT */
+        
+        var bg = new K2.Background({
+            ID: 'background',
+            image: backgroundImage,
+            top: 0,
+            left: 0
+        });
     
-    /* KNOB INIT */
-   var knobArgs = {
-        ID: "pitch_knob",
-        left: 87 ,
-        top: 176,
-        image : knobImage,
-        sensitivity : 5000,
-        initAngValue: 270,
-        startAngValue: 218,
-        stopAngValue: 501,
-        /* knobMethod: 'updown', */
-        onValueSet: function (slot, value) {
-            var shift_value = value * (1.5) + 0.5;
-            /* shift argument is like a play rate */
-            /* We want 0 -> 0.5, 0.5 -> 1, 1 -> 2 */
-            /* Let's calculate the semitones */
-            var semitoneShift =  K2.MathUtils.linearRange (0, 1, -12, 12, value);
-            /* Let's calculate the "play rate" */
-            var shift_value = Math.pow(1.0595, semitoneShift);
-            this.shiftValue = shift_value;
-            console.log ('Shift value set to ', value, this.shiftValue);
-            this.ui.refresh();
-        }.bind(this),
-        isListening: true
+        this.ui.addElement(bg, {zIndex: 0});
+        
+        /* KNOB INIT */
+       var knobArgs = {
+            ID: "pitch_knob",
+            left: 87 ,
+            top: 176,
+            image : knobImage,
+            sensitivity : 5000,
+            initAngValue: 270,
+            startAngValue: 218,
+            stopAngValue: 501,
+            /* knobMethod: 'updown', */
+            onValueSet: function (slot, value) {
+                var shift_value = value * (1.5) + 0.5;
+                /* shift argument is like a play rate */
+                /* We want 0 -> 0.5, 0.5 -> 1, 1 -> 2 */
+                /* Let's calculate the semitones */
+                var semitoneShift =  K2.MathUtils.linearRange (0, 1, -12, 12, value);
+                /* Let's calculate the "play rate" */
+                var shift_value = Math.pow(1.0595, semitoneShift);
+                this.shiftValue = shift_value;
+                console.log ('Shift value set to ', value, this.shiftValue);
+                this.ui.refresh();
+            }.bind(this),
+            isListening: true
+        };
+        
+        this.ui.addElement(new K2.RotKnob(knobArgs));
+        this.ui.setValue({elementID: "pitch_knob", value: 0.5});
+        this.ui.refresh();
+  };
+  
+    /* This function gets called by the host every time an instance of
+       the plugin is requested [e.g: displayed on screen] */        
+    var initPlugin = function(initArgs) {
+        var args = initArgs;
+        require ([  'https://github.com/corbanbrook/dsp.js/raw/master/dsp.js',
+                    'https://github.com/janesconference/KievII/raw/master/dsp/pitchshift.js',
+                    'image!'+ require.toUrl('./assets/images/Voron_bg2.png'),
+                    'image!'+ require.toUrl('./assets/images/white_big.png')],
+                    function () {
+                        var resources = arguments;
+                        pluginFunction.call (this, args, resources);
+                    }.bind(this)
+                );
     };
     
-    this.ui.addElement(new K2.RotKnob(knobArgs));
-    this.ui.setValue({elementID: "pitch_knob", value: 0.5});
-    this.ui.refresh();
-  };
-  return {
-    initPlugin: initPlugin,
-    pluginConf: pluginConf
-  };
+    return {
+        initPlugin: initPlugin,
+        pluginConf: pluginConf
+    };
 });
