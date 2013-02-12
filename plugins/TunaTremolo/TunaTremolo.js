6453179115ecec3043e7a670dd5374cfdc2ebfb6
Cristiano Belloni <janesconference@gmail.com>
2013-02-12 11:13:45 +0100

    Added Tuna Effect Tremolo

--- plugins/TunaTremolo/TunaTremolo.js
+++ plugins/TunaTremolo/TunaTremolo.js
@@ -2,7 +2,7 @@ define(['require'], function(require) {
   
   var pluginConf = {
       osc: true,
-      audioOut: 2,
+      audioOut: 1,
       audioIn: 1,
       canvas: false,
   };
@@ -13,16 +13,21 @@ define(['require'], function(require) {
         this.name = args.name;
         this.id = args.id;
         this.audioSource = args.audioSources[0];
-        this.audioDestinations = args.audioDestinations;
+        this.audioDestination = args.audioDestinations[0];
         this.context = args.audioContext;
-		this.gainDuplicatorNodes = [];
-
-	    for (var i = 0; i < this.audioDestinations.length; i+=1) {
-	        this.gainDuplicatorNodes[i] = this.context.createGainNode();
-			this.audioSource.connect(this.gainDuplicatorNodes[i]);
-			this.gainDuplicatorNodes[i].connect(this.audioDestinations[i]);
-		}
         
+        var tuna = new Tuna(this.context);
+        
+        var tremolo = new tuna.Tremolo({
+                  intensity: 0.3,    //0 to 1
+                  rate: 0.1,         //0.001 to 8
+                  stereoPhase: 0,    //0 to 180
+                  bypass: 0
+              });
+    
+       this.audioSource.connect(tremolo.input);
+       tremolo.connect(this.audioDestination); 
+
     };
     
     return {
