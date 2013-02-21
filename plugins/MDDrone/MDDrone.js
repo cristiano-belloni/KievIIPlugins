831dd88ebc1d4fef773a8428afc2213076ae864f
Cristiano Belloni <janesconference@gmail.com>
2013-02-21 12:29:52 +0100

    Drone working (without parameter setting)

--- plugins/MDDrone/MDDrone.js
+++ plugins/MDDrone/MDDrone.js
@@ -13,68 +13,67 @@ define(['require'], function(require) {
         this.id = args.id;
         this.audioDestination = args.audioDestinations[0];
         this.context = args.audioContext;
-        var context = this.context;
         /* From 40 to 100 */
         this.baseNote = 44;
         /* From 1 to 40 */
         this.nOsc = 14;
         
         //connect gain
-        var gain = context.createGainNode();
-        gain.gain.value = 15.0;
-        gain.connect(this.audioDestination);
+        this.gainNode = this.context.createGainNode();
+        this.gainNode.gain.value = 15.0;
+        this.gainNode.connect(this.audioDestination);
         
-        var noiseNodes = [];
-        var bufferLen = 4096;
+        this.noiseNodes = [];
+        this.bufferLen = 4096;
         
-        function createNoiseGen(freq) {
-          var panner = context.createPanner();
+        this.createNoiseGen = function (freq) {
+          this.panner = this.context.createPanner();
           var max = 20;
           var min = -20;
           var x = rand(min, max);
           var y = rand(min, max);
           var z = rand(min, max);
-          panner.setPosition(x, y, z);
-          panner.connect(gain);
+          this.panner.setPosition(x, y, z);
+          this.panner.connect(this.gainNode);
         
-          var filter = context.createBiquadFilter();
+          var filter = this.context.createBiquadFilter();
           filter.type = filter.BANDPASS;
           filter.frequency.value = freq;
           filter.Q.value = 150;
-          filter.connect(panner);
+          filter.connect(this.panner);
         
-          var noiseSource = context.createJavaScriptNode(bufferLen, 1, 2);
+          var noiseSource = this.context.createJavaScriptNode(this.bufferLen, 1, 2);
           noiseSource.onaudioprocess = function (e) {
             var outBufferL = e.outputBuffer.getChannelData(0);
             var outBufferR = e.outputBuffer.getChannelData(1);
-            for (var i = 0; i < bufferLen; i++) {
+            for (var i = 0; i < this.bufferLen; i++) {
               outBufferL[i] = outBufferR[i] = Math.random() * 2 - 1;
             }
-          };
+          }.bind(this);
           noiseSource.connect(filter);
-          noiseNodes.push(noiseSource);
+          this.noiseNodes.push(noiseSource);
         
           setInterval(function () {
             x = x + rand(-0.1, 0.1);
             y = y + rand(-0.1, 0.1);
             z = z + rand(-0.1, 0.1);
-            panner.setPosition(x, y, z);
-          }, 500);
+            this.panner.setPosition(x, y, z);
+          }.bind(this), 500);
         
         }
         
-        var scale = [0.0, 2.0, 4.0, 6.0, 7.0, 9.0, 11.0, 12.0, 14.0];
+        this.scale = [0.0, 2.0, 4.0, 6.0, 7.0, 9.0, 11.0, 12.0, 14.0];
         
-        function generate(){
+        this.generate = function () {
           var base_note = this.baseNote;
           var num_osc = this.nOsc;
           for (var i = 0; i < num_osc; i++) {
-            var degree = Math.floor(Math.random() * scale.length);
-            var freq = mtof(base_note + scale[degree]);
+            var degree = Math.floor(Math.random() * this.scale.length);
+            var freq = mtof(base_note + this.scale[degree]);
             freq += Math.random() * 4 - 2;
-            createNoiseGen(freq);
+            this.createNoiseGen(freq);
           }
-        }
+        };
         
         function mtof(m) {
           return Math.pow(2, (m - 69) / 12) * 440;
@@ -84,14 +83,14 @@ define(['require'], function(require) {
           return Math.random() * (max - min) + min;
         }
         
-        function reset(){
-          while (noiseNodes.length){
-            noiseNodes.pop().disconnect();
+        this.reset = function () {
+          while (this.noiseNodes.length){
+            this.noiseNodes.pop().disconnect();
           }
-          generate();
+          this.generate();
         }
         
-        generate();
+        this.generate();
     };
   
     var initPlugin = function(initArgs) {
