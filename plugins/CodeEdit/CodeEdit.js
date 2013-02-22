0e29825c1de7d8ad4c0d8eac205ff9f2c30d320b
Cristiano Belloni <janesconference@gmail.com>
2013-02-22 16:39:24 +0100

    workish CodeEdit using ACE

--- plugins/CodeEdit/CodeEdit.js
+++ plugins/CodeEdit/CodeEdit.js
@@ -19,23 +19,43 @@ define(['require'], function(require) {
         this.audioSource = args.audioSources[0];
         this.audioDestination = args.audioDestinations[0];
         this.context = args.audioContext;
-        var myTextArea = document.createElement("textarea");
-        //var button = document.create;
-        myTextArea.name = "post";
-        myTextArea.maxLength = "5000";
-        myTextArea.cols = "80";
-        myTextArea.rows = "40";
-        //div.appendChild(input); //appendChild
-        //div.appendChild(button);
+        
+         /* This is an hack! */
+        
+        var myTextArea = document.createElement("div");
+        myTextArea.id = "CodeEditTextArea";
+        
+        myTextArea.style.width = canvas.width + "px";
+        myTextArea.style.height = canvas.height + "px";
+ 
         args.canvas.parentNode.replaceChild(myTextArea, args.canvas);
+        
+        var pn = myTextArea;
+        
+        var editor = ace.edit(pn);
+        editor.setFontSize("14px");
+        editor.setTheme("ace/theme/mono_industrial");
+        editor.getSession().setMode("ace/mode/javascript");
         
     };
     
     
     var initPlugin = function(initArgs) {
         var args = initArgs;
-        require ([  require.toUrl('/codemirror-3.1/lib/codemirror.js'),
-                    require.toUrl('/codemirror-3.1/javascript/javascript.js') ]),
+        
+        function loadCss(url) {
+            var link = document.createElement("link");
+            link.type = "text/css";
+            link.rel = "stylesheet";
+            link.href = url;
+            document.getElementsByTagName("head")[0].appendChild(link);
+        }
+        
+        //loadCss (require.toUrl('./codemirror-3.1/lib/codemirror.css'));
+        
+        require ([  require.toUrl('assets/js/codemirror-compressed.js'),
+                    'http://d1n0x3qji82z53.cloudfront.net/src-min-noconflict/ace.js'
+                 ],
                     function () {
                         var resources = arguments;
                         pluginFunction.call (this, args, resources);
