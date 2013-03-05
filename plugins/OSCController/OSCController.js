09a211139124fa9ee1d9560115d090159ac5ab73
Cristiano Belloni <janesconference@gmail.com>
2013-03-05 13:46:32 +0100

    Added a function to create a modal div on top of the canvas

--- plugins/OSCController/OSCController.js
+++ plugins/OSCController/OSCController.js
@@ -3,8 +3,8 @@ define(['require'], function(require) {
     /* This gets returned to the host as soon as the plugin is loaded */ 
     var pluginConf = {
         osc: true,
-        audioIn: 4,
-        audioOut: 1,
+        audioIn: 1,
+        audioOut: 0,
         canvas: {
             width: 520,
             height: 520
@@ -13,6 +13,50 @@ define(['require'], function(require) {
   
     /* This gets called when all the resources are loaded */
     var pluginFunction = function (args, resources) {
+        
+        this.canvas = args.canvas;
+        
+        this.createModalWindow = function () {
+            var canvas = $(this.canvas);
+            var padding = 5;
+            var border = 2;
+            var top = canvas[0].offsetTop;
+            var zindex = canvas.css("z-index") + 1;
+            var offset = padding * 2 + border;
+            var CWidth = canvas.width();
+            var CHeight = canvas.height();
+            
+            var modalStyle = "position:absolute; left:0px; top:" + top + "px; width:" + (CWidth - offset) + "px; height:" + (CHeight - offset) + "px; z-index:" + zindex + "; background-color: rgba(0, 0, 0, 0.77); color: whitesmoke; border: " + border + "px solid whitesmoke; padding: " + padding + "px";
+            
+            // Modal
+            var modalHTML = '<div style="' + modalStyle + '">\
+                <form>\
+                    <fieldset>\
+                        <legend style="color:whitesmoke">Controller parameters</legend>\
+                        <label>Label name</label>\
+                        <input type="text" placeholder="Label name...">\
+                        <label>Control value range</label>\
+                        <input class="input-mini" type="text" placeholder="0"><span class="help-inline"> Start value   </span>\
+                        <input class="input-mini" type="text" placeholder="1"><span class="help-inline"> End value   </span>\
+                        <label>Send OSC to:</label>\
+                        <select>\
+                            <option>1</option>\
+                            <option>2</option>\
+                            <option>3</option>\
+                            <option>4</option>\
+                            <option>5</option>\
+                        </select>\
+                        <hr/>\
+                        <button type="submit" class="btn">Ok</button>\
+                    </fieldset>\
+                 </form>\
+             </div>';
+            
+            var modalElement = $(modalHTML);
+            var parent = canvas.parent();
+            parent.append(modalHTML);
+        }
+		
 		
         // The canvas part
         this.ui = new K2.UI ({type: 'CANVAS2D', target: args.canvas});
