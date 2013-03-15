45283194fa86bad21064c9986567d575b77e1e8d
Cristiano Belloni <janesconference@gmail.com>
2013-03-15 17:07:08 +0100

    Reading OSC plugins list from the interface. Revamped input mask Added event handlers for buttons

--- plugins/OSCController/OSCController.js
+++ plugins/OSCController/OSCController.js
@@ -15,9 +15,32 @@ define(['require'], function(require) {
     var pluginFunction = function (args, resources) {
         
         this.canvas = args.canvas;
+		this.model = {};
+		this.hostInterface = args.K2HInterface;
         
-        this.createModalWindow = function () {
+        this.createModalWindow = function (id) {
+			console.log ("Element ID is: ", id);
+			var description = this.model[id].description;
+			var startValue = this.model[id].range[0];
+			var endValue = this.model[id].range[1];
+			
+			var oscList = this.hostInterface.getOSCEnabled();
+			var selectMarkup = '<select>';
+			selectMarkup += '<option> Remote </option>';
+			
+			for (var i = 0; i < oscList.length; i+=1) {
+                    selectMarkup += '<option>' + oscList[i].displayName + '</option>';
+			}
+			selectMarkup += '</select>';
+			
             var canvas = $(this.canvas);
+			
+			var okHandler = function (event) {
+				// Remove the dialog
+				$(this.canvas).siblings(".oscControllerModalDialog").remove();
+				console.log (event.data.button_type);
+			}
+			
             var padding = 5;
             var border = 2;
             var top = canvas[0].offsetTop;
@@ -26,35 +49,33 @@ define(['require'], function(require) {
             var CWidth = canvas.width();
             var CHeight = canvas.height();
             
-            var modalStyle = "position:absolute; left:0px; top:" + top + "px; width:" + (CWidth - offset) + "px; height:" + (CHeight - offset) + "px; z-index:" + zindex + "; background-color: rgba(0, 0, 0, 0.77); color: whitesmoke; border: " + border + "px solid whitesmoke; padding: " + padding + "px";
+            var modalStyle = "position:absolute; left:0px; top:" + top + "px; width:" + (CWidth - offset) + "px; height:" + (CHeight - offset) + "px; z-index:" + zindex + "; background-color: rgba(0, 0, 0, 0.88); color: whitesmoke; border: " + border + "px solid whitesmoke; padding: " + padding + "px";
             
             // Modal
-            var modalHTML = '<div style="' + modalStyle + '">\
+            var modalHTML = '<div class= "oscControllerModalDialog" style="' + modalStyle + '">\
                 <form>\
                     <fieldset>\
-                        <legend style="color:whitesmoke">Controller parameters</legend>\
-                        <label>Label name</label>\
-                        <input type="text" placeholder="Label name...">\
-                        <label>Control value range</label>\
-                        <input class="input-mini" type="text" placeholder="0"><span class="help-inline"> Start value   </span>\
-                        <input class="input-mini" type="text" placeholder="1"><span class="help-inline"> End value   </span>\
-                        <label>Send OSC to:</label>\
-                        <select>\
-                            <option>1</option>\
-                            <option>2</option>\
-                            <option>3</option>\
-                            <option>4</option>\
-                            <option>5</option>\
-                        </select>\
+                        <legend style="color:whitesmoke">Controller parameters / ' + description + '</legend>\
+                        <label>Control value range:</label>\
+                        <input class="input-mini" type="number" step="any" placeholder="' + startValue + '"><span class="help-inline"> Start value   </span>\
+                        <input class="input-mini" type="number" step="any" placeholder="' + endValue + '"><span class="help-inline"> End value   </span>\
+                        <label>Destination:</label>' + selectMarkup + '\
+                        <label>Path:</label>\
+                        <input type="text" placeholder="/path/to/control/">\
                         <hr/>\
-                        <button type="submit" class="btn">Ok</button>\
+                        <button type="button" class="btn canc_button">Cancel</button>\
+                        <button type="button" class="btn ok_button">Ok</button>\
                     </fieldset>\
                  </form>\
              </div>';
             
             var modalElement = $(modalHTML);
+			
             var parent = canvas.parent();
             parent.append(modalHTML);
+			
+			canvas.siblings(".oscControllerModalDialog").on ("click", ".ok_button", { button_type: "ok" }, okHandler.bind(this));
+			canvas.siblings(".oscControllerModalDialog").on ("click", ".canc_button", { button_type: "canc" }, okHandler.bind(this));
         }
 		
 		
@@ -72,18 +93,36 @@ define(['require'], function(require) {
     
         this.ui.addElement(bgArgs, {zIndex: 0});
 		
+		var editString = "-edit";
+		
 		var nKNobs = 6;
 		var nButtons = 12;
 		var buttonsPerRow = 6;
 		
 		var knobImage = resources[0];
 		var buttonImages = [resources[4], resources[5]];
+		var buttonEditImages = [resources[1], resources[2]];
 		var knobLeft = 18;
 		var knobSpacing = 84;
 		var buttonLeft = 18;
 		var buttonSpacing = 84;
 		var buttonTop = 140;
 		var buttonYSpacing = 120;
+		
+		 var buttonEditArgs = {
+	            ID: "",
+	            left: 0,
+	            top: 0,
+	            mode: 'immediate',
+	            imagesArray : buttonEditImages,
+	            onValueSet: function (slot, value, element) {
+					if (value === 1) {
+						var id = element.split(editString)[0];
+						this.createModalWindow(id);
+					}
+	            }.bind(this),
+	            isListening: true
+		 }
 		
         var knobArgs = {
              imagesArray : [knobImage],
@@ -103,13 +142,21 @@ define(['require'], function(require) {
              knobArgs.ID = 'knob' + i;
              knobArgs.left = (knobLeft + i * knobSpacing);
              this.ui.addElement(new K2.Knob(knobArgs));
+			 buttonEditArgs.ID = knobArgs.ID + editString;
+			 buttonEditArgs.left = knobArgs.left + 25;
+			 buttonEditArgs.top = knobArgs.top + knobArgs.tileHeight + 12;
+			 this.ui.addElement(new K2.Button(buttonEditArgs));
+			 var modelEntry = this.model[knobArgs.ID] = {};
+			 modelEntry.description = "Knob " + (i + 1);
+			 modelEntry.range = [0,1]; 
+			 modelEntry.oscDest = null;
+			 modelEntry.oscPath = '';
 		 }
 		 
 		 var buttonArgs = {
              ID: "",
              left: 0,
              top: 0,
-             /*mode: 'immediate',*/
              imagesArray : buttonImages,
              onValueSet: function (slot, value) {
                  this.ui.refresh();
@@ -121,6 +168,16 @@ define(['require'], function(require) {
              buttonArgs.left = (buttonLeft + (i % buttonsPerRow) * buttonSpacing);
 			 buttonArgs.top = buttonTop + Math.floor(i / buttonsPerRow) * buttonYSpacing;
              this.ui.addElement(new K2.Button(buttonArgs));
+			 buttonEditArgs.ID = buttonArgs.ID + "-edit";
+			 buttonEditArgs.left = buttonArgs.left + 28;
+			 buttonEditArgs.top = buttonArgs.top + buttonArgs.imagesArray[0].height + 5;
+			 this.ui.addElement(new K2.Button(buttonEditArgs));
+			 var modelEntry = this.model[buttonArgs.ID] = {};
+			 modelEntry.description = "Button " + (i + 1);
+			 modelEntry.range = [0,1]; 
+			 modelEntry.oscDest = null;
+			 modelEntry.oscPath = '';
+			 modelEntry.persistent = false;
 		 }
 		 
 		 this.ui.refresh();
